cc52f59daa3fb2d900119dc90b687aed58f220b0
Ibramove <ibrahimasyll39@gmail.com>
2025-09-17 16:50:21 +0200

    Initial commit: simple Node.js app

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Bienvenue sur mon app Node.js simple avec OpenShift</h1>");
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));
